e034aa44be7d325f88342dd54bea66dc35d5c389
lduynkerke <lduynkerke@gmail.com>
2024-09-26 18:00:17 +0200

    Improve plot and add bash scripts from DelftBlue to the folder

--- ASE/plot_folder.py
+++ ASE/plot_folder.py
@@ -61,12 +61,13 @@ def plot_simulation_data(base_path, sim_type, title, strain_surfaces, temperatur
             data = load_data(base_path, strain_surface, sim_type, temp_folder)
             ax = axes[i, j]
             if not data.empty:
-                ax.scatter(data['Extracted Energy'], data['Predicted Energy'])
+                length = data.shape[0]
+                ax.scatter(data['Extracted Energy']/length, data['Predicted Energy']/length)
                 ax.set_title(f'{strain_surface} - {temp_folder}')
                 if i == 2:
-                    ax.set_xlabel('Extracted Energy')
+                    ax.set_xlabel('DFT Energy / atom')
                 if j == 0:
-                    ax.set_ylabel('Predicted Energy')
+                    ax.set_ylabel('MACE Energy / atom')
                 ax.set_xlim(-5528, -5524)
                 ax.set_ylim(-3570, -3500)
                 ax.set_xticks([-5528, -5527, -5526, -5525, -5524])
@@ -80,7 +81,8 @@ def plot_simulation_data(base_path, sim_type, title, strain_surfaces, temperatur
     return fig
 
 
-def plot_simulation_data_one(base_path, sim_types, title, strain_surfaces, temperature_folders):
+def plot_simulation_data_one(base_path, sim_types, sim_type_legends, title, strain_surfaces, strain_surface_names,
+                         temperature_folders, temp_folder_names):
     """
     Generates scatter plots comparing 'Extracted Energy' and 'Predicted Energy' for different simulations.
 
@@ -91,12 +93,18 @@ def plot_simulation_data_one(base_path, sim_types, title, strain_surfaces, tempe
     :type base_path: str
     :param sim_types: A list of simulation type folders to analyze.
     :type sim_types: list
+    :param sim_type_legends: A list of legends corresponding to each sim_type.
+    :type sim_type_legends: list
     :param title: The title for the figure.
     :type title: str
     :param strain_surfaces: The names of the strain surface folders to analyze.
     :type strain_surfaces: list
+    :param strain_surface_names: A list of custom names for the strain surfaces.
+    :type strain_surface_names: list
     :param temperature_folders: The names of the temperature folders to analyze.
     :type temperature_folders: list
+    :param temp_folder_names: A list of custom names for the temperature folders.
+    :type temp_folder_names: list
     :return: A matplotlib figure containing the scatter plots.
     :rtype: matplotlib.figure.Figure
     """
@@ -115,28 +123,35 @@ def plot_simulation_data_one(base_path, sim_types, title, strain_surfaces, tempe
                 ax = axes[i, j]
 
                 if not data.empty:
-                    ax.scatter(data['Extracted Energy'], data['Predicted Energy'], color=color, label=sim_type)
-                    ax.set_title(f'{strain_surface} - {temp_folder}')
+                    ax.scatter(data['Extracted Energy']/505, data['Predicted Energy']/505, color=color,
+                               label=sim_type_legends[sim_idx])
+
+                    # Set custom titles using strain_surface_names and temp_folder_names
+                    strain_surface_name = strain_surface_names[i] if i < len(strain_surface_names) else strain_surface
+                    temp_folder_name = temp_folder_names[j] if j < len(temp_folder_names) else temp_folder
+                    ax.set_title(f'{strain_surface_name} - {temp_folder_name}')
 
                     # Only add labels on the outer axes
                     if i == 2:
-                        ax.set_xlabel('Extracted Energy')
+                        ax.set_xlabel('DFT Energy / atom')
                     if j == 0:
-                        ax.set_ylabel('Predicted Energy')
+                        ax.set_ylabel('MACE Energy / atom')
 
-                    ax.set_xlim(-5528, -5524)
-                    ax.set_ylim(-3570, -3500)
-                    ax.set_xticks([-5528, -5527, -5526, -5525, -5524])
-                    ax.set_yticks([-3570, -3560, -3550, -3540, -3530, -3520, -3510, -3500])
+                    ax.set_xlim(-10.947, -10.938)
+                    ax.set_ylim(-7.08, -6.94)
+                    ax.set_xticks([-10.946, -10.944, -10.942, -10.94, -10.938])
+                    ax.set_yticks([-7.08, -7.06, -7.04, -7.02, -7.0, -6.98, -6.96, -6.94])
                 else:
                     ax.text(0.5, 0.5, 'No Data', horizontalalignment='center', verticalalignment='center')
-                    ax.set_title(f'{strain_surface} - {temp_folder}')
+                    strain_surface_name = strain_surface_names[i] if i < len(strain_surface_names) else strain_surface
+                    temp_folder_name = temp_folder_names[j] if j < len(temp_folder_names) else temp_folder
+                    ax.set_title(f'{strain_surface_name} - {temp_folder_name}')
 
-                ax.grid(True)
+                ax.grid(False)
 
     # Add legends to the axes
     for ax in axes.flatten():
-        ax.legend()
+        ax.legend(loc=2)
 
     plt.tight_layout(rect=(0.0, 0.03, 1.0, 0.95))  # Adjust layout to make space for the title
     return fig
@@ -153,34 +168,40 @@ def main():
     :return: None
     """
     base_dir = r"../Results"
-    strain_surfaces = ["min_strain_surface", "av_strain_surface", "max_strain_surface"]
     simulation_types = ["Adsorption_Complex_Simulations", "Active_ZrH_site_simulations"]
+    simulation_types_legend = ["AC_Sim", "ZrH_Sim"]
+    strain_surfaces = ["min_strain_surface", "av_strain_surface", "max_strain_surface"]
+    strain_surfaces_names = ["s_min", "s_av", 's_max']
     temperature_folders = ["aiMD_353K", "aiMD_773K", "Velocity_softening_dynamics"]
+    temperature_folders_names = ["353K", "773K", "VSD"]
 
     # Create and save figures
     fig0 = plot_simulation_data_one(base_path=base_dir,
                                     sim_types=simulation_types,
+                                    sim_type_legends=simulation_types_legend,
                                     title="Results MACE-MP-0",
                                     strain_surfaces=strain_surfaces,
-                                    temperature_folders=temperature_folders
+                                    strain_surface_names=strain_surfaces_names,
+                                    temperature_folders=temperature_folders,
+                                    temp_folder_names=temperature_folders_names
                                     )
 
-    fig1 = plot_simulation_data(base_path=base_dir,
-                                sim_type=simulation_types[0],
-                                title="Adsorption Complex Simulations",
-                                strain_surfaces=strain_surfaces,
-                                temperature_folders=temperature_folders
-                                )
-
-    fig2 = plot_simulation_data(base_path=base_dir,
-                                sim_type=simulation_types[1],
-                                title="Active ZrH Site Simulations",
-                                strain_surfaces=strain_surfaces,
-                                temperature_folders=temperature_folders
-                                )
+    # fig1 = plot_simulation_data(base_path=base_dir,
+    #                             sim_type=simulation_types[0],
+    #                             title="Adsorption Complex Simulations",
+    #                             strain_surfaces=strain_surfaces,
+    #                             temperature_folders=temperature_folders
+    #                             )
+    #
+    # fig2 = plot_simulation_data(base_path=base_dir,
+    #                             sim_type=simulation_types[1],
+    #                             title="Active ZrH Site Simulations",
+    #                             strain_surfaces=strain_surfaces,
+    #                             temperature_folders=temperature_folders
+    #                             )
     fig0.savefig(os.path.join(base_dir, 'MACE-MP-0.png'))
-    fig1.savefig(os.path.join(base_dir, 'Adsorption_Complex_Simulations_MACE0_Autoscale.png'))
-    fig2.savefig(os.path.join(base_dir, 'Active_ZrH_Site_Simulations_MACE0_Autoscale.png'))
+    # fig1.savefig(os.path.join(base_dir, 'Adsorption_Complex_Simulations_MACE0_Autoscale.png'))
+    # fig2.savefig(os.path.join(base_dir, 'Active_ZrH_Site_Simulations_MACE0_Autoscale.png'))
 
     # Show the figures
     plt.show()
--- README.md
+++ README.md
@@ -3,10 +3,12 @@ This repo contains code for my MEP.
 
 ## Contents
 - **ASE**: Folder containing scripts to process energy and forces using ASE:
-  - process_energy_mpi.py: parallelized script to load structures and calculate structure energy.
-  - submit_slurm_jobs.py: script to scan a data folder and create slurm jobs for each XYZ file.
-  - merge_csvs.py: script to merge and rename CSV files within the same directory into one single CSV.
-  - plot_folder.py: script to gather all CSVs from a data folder and create matplotlib PNG plots.
+  - `submit_jobs.sh`: bash script that calls `submit_slurm_jobs.py` to create jobs.
+  - `postprocess.sh`: bash script that calls `merge_csvs.py` to rename and merge csv-files, making them ready to be plotted. Next, it calls plot_folder.py to generate plots.
+  - `process_energy_mpi.py`: parallelized script to load structures and calculate structure energy.
+  - `submit_slurm_jobs.py`: script to scan a data folder and create slurm jobs for each XYZ file.
+  - `merge_csvs.py`: script to merge and rename CSV files within the same directory into one single CSV.
+  - `plot_folder.py`: script to gather all CSVs from a data folder and create matplotlib PNG plots.
 
 - **MACE**:
 - **Results**: Folder containing results, similarly structured to data folder.
